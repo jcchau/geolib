b3dc9a12977ebe04119dc92c7966b5518c2f529b
Jimmy C. Chau <jchau@bu.edu>
2015-07-29 17:38:48 -0400

    Created a new implementation of Polygon.getPlane().
    This new implementation chooses the largest cross-product between edge vectors as the normal vector.
    
    Not yet tested.

--- @Polygon/getPlane.m
+++ @Polygon/getPlane.m
@@ -1,44 +1,43 @@
 function [point, normal] = getPlane(obj)
 % getPlane returns the point and normal vector that describes the plane on
 %   which the polygon lies.
+%
+%   [POINT, NORMAL] = getPlane(OBJ)
+%
+%   OBJ is the Polygon object.
+%   POINT is a point on the polygon's plane.
+%   NORMAL is the unit normal vector.
 
 vertices = obj.toMatrix();
 
-% (arbitrarily) choose the first point as the point to return
+% The first point on the polygon is as good as any other point on the
+% plane.
 point = vertices(1,:);
 
-vector1 = vertices(2,:) - vertices(1,:);
-vector2 = vertices(end,:) - vertices(1,:);
+% Find the vector from each vertex to the next via subtraction.
+end_vertices = [vertices(2:end,:); vertices(1,:)];
+edge_vectors = end_vertices - vertices;
 
-% calculate the normal as the cross product between the vectors formed by
-% the edges that connect to the first vertex.
-normal = cross(vector1, vector2);
+% For each vertex, cross the edge vectors that connect to it to find the
+% normal vector.
+previous_edge = [edge_vectors(end,:); edge_vectors(1:end-1,:)];
+next_edge = edge_vectors;
 
-%% Check in case the two edges are parallel
-numvertices = size(vertices,1);
-% start with the 2nd vertex since we already tried the first
-vertex_index = 2;
-while(dot(normal, normal) == 0 && vertex_index < numvertices)
-    % The normal vector has zero magnitude: vector1 and vector2 are
-    % parallel.
-    % Try the edges around the remaining vertices;
-    % The last iteration is for vertex_index == (numvertices-1) because
-    %   - The code will be different to calculate the vector from the last
-    %   vertex to the first vertex; and
-    %   - If the edges around every other vertex are parallel, then the
-    %   edges around the last vertex will also be parallel.  
-    vector1 = vertices(vertex_index-1,:) - vertices(vertex_index,:);
-    vector2 = vertices(vertex_index+1,:) - vertices(vertex_index,:);
-    
-    normal = cross(vector1, vector2);
+all_normals = cross(previous_edge, next_edge);
+
+% Calculate the magnitude of each normal vector; assume the largest
+% cross-product gives us the most precise normal vector.  If the
+% cross-product is zero, then the edges are parallel.
+magnitudes = sqrt(...
+    all_normals(:,1).^2 + all_normals(:,2).^2 + all_normals(:,3).^2);
+chosen_normal = find(magnitudes==max(magnitudes));
+
+if(magnitudes(chosen_normal) == 0)
+    error('Error: Error: All edges of the polygon are parallel.')
 end
 
-if(dot(normal, normal) == 0)
-    error('Error: All edges of the polygon are parallel.')
-end
-
-% normalize the normal vector to a unit vector
-magnitude = norm(normal);
-normal = normal ./ magnitude;
+% Normalize the chosen normal vector to unit length
+normal = all_normals(chosen_normal,:) ./ magnitudes(chosen_normal);
 
 end
+
