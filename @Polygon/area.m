4133dc665e9e04bc324f8285b73d86a7b7c164a8
Jimmy C. Chau <jchau@bu.edu>
2015-08-04 18:43:26 -0400

    Added a note to the documentation for Polygon.area that the method assumes that edges don't intersect.

--- @Polygon/area.m
+++ @Polygon/area.m
@@ -1,5 +1,8 @@
 function a = area(obj)
 % AREA returns the area of the Polygon.
+%
+% NOTE: this method uses the MATLAB method polyarea, which assumes that the
+% polygon edges do not intersect.  
 
 [x, y] = obj.to2D();
 
